e9187a35b1ab94c817b45347a5ac016221554e85
pabloIO <pabloIO>
2018-05-03 01:42:54 -0400

    succesful connection python-js

--- static/chat.js
+++ static/chat.js
@@ -1,7 +1,7 @@
 (function(d, io){
     'use strict';
 
-    var io = io(),
+    var io = io.connect('http://localhost:3000'),
         chatForm = d.querySelector('#chat-form'),
         messageText = d.querySelector('#message-text'),
         chat = d.querySelector('#chat');
@@ -13,13 +13,18 @@
         return false;
     }
 
+    io.on('connect', function(){
+        console.log('succesfull python connection');
+        io.emit('message', {data: 'CONNECTION JS-PYTHON'})
+    });
+
     io.on('new user', function(newUser){
         alert(newUser.message);
-    })
+    });
 
     io.on('user says', function(userSays){
         chat.insertAdjacentHTML('beforeend', '<li>'+userSays+'</li>');
-    })
+    });
 
     io.on('bye bye user', function(byeByeUser){
         alert(byeByeUser.message);
